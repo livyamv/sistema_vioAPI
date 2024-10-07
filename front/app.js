c1900bb2878d65dd856e3e02856204db2818e93e
livyamv <liviareis2702@gmail.com>
2024-10-07 15:53:32 -0300

    first commit

--- front/app.js
+++ front/app.js
@@ -2,4 +2,13 @@
 //Seleciona o elemento com o id indicado do formulário
 document
 .getElementById("formulário-registro")
-.addEventListener("submit")
+//adiciona o ouvinte de evento(submit)para capturar o envio do formulário
+.addEventListener("submit", function(event){
+//previne o comportamento padrão do formulário, ou seja, impede que ele saja enviado e recarregue a página
+event.preventDefault()
+//Captura os valores dos compos de formulario
+const nome = document.getElementById("nome");
+const cpf = document.getElementById("cpf");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+});
