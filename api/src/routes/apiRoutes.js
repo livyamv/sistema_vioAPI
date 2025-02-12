8f1ceb0fc3327798cd68b3b1632b981fd017a171
liviareis2702@gmail.com <liviareis2702@gmail.com>
2025-02-12 12:57:26 -0300

    12/02

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 
 const userController = require("../controllers/userController")
-
+//usuario
 router.post('/user', userController.createUser);
 router.get('/user', userController.getAllUsers);
 router.put('/user', userController.updateUser);
 router.delete('/user/:cpf', userController.deleteUser);
+router.post('/login', userController.loginUser);
 
 const organizadorController = require("../controllers/organizadorController")
-
+//organizador
 router.post('/organizador', organizadorController.createOrganizador);
 router.get('/organizador', organizadorController.getAllOrganizador);
 router.put('/organizador', organizadorController.updateOrganizador);
