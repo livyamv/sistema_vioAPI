f72a377fd967cfb242231c49d058e26e768d8451
livyamv <liviareis2702@gmail.com>
2025-03-05 08:58:28 -0300

    first commit

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require ('mysql2');//Importando o Mysql que instalou "npm i mysql2
 
 const pool= mysql.createPool({
     connectionLimit:10,
-    host:'10.89.240.80',
+    host:'localhost',
     user:'alunods',
     password:'senai@604',
     database:'vio_livia'
