61e2a6f7b6dd00f59a3f0486ec725caf37cd1618
livyamv <liviareis2702@gmail.com>
2025-05-19 13:27:51 -0300

    docker

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,50 +1,13 @@
 require('dotenv').config();
+const mysql = require('mysql2');
 
-const mysql = require ('mysql2');//Importando o Mysql que instalou "npm i mysql2"
 
-const pool= mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit:10,
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
-})
-module.exports = pool 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = pool;
