3523869d9e662e9bb36fced34535f30036288269
livyamv <liviareis2702@gmail.com>
2025-06-02 11:29:21 -0300

    02/06

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,35 +1,40 @@
-const router = require('express').Router()
-const verifyJWT = require('../services/verifyJWT');
-const userController = require("../controllers/userController")
+const router = require("express").Router();
+const verifyJWT = require("../services/verifyJWT");
+const userController = require("../controllers/userController");
+const organizadorController = require("../controllers/organizadorController");
+const ingressoController = require("../controllers/ingressoController");
+const eventoController = require("../controllers/eventoController");
+const compraController = require("../controllers/compraController");
+
 //usuario
-router.post('/user', userController.createUser);
-router.get('/user',verifyJWT, userController.getAllUsers);//
-router.put('/user', userController.updateUser);
-router.delete('/user/:cpf', userController.deleteUser);
-router.post('/login', userController.loginUser);
+router.post("/user", userController.createUser);
+router.get("/user", verifyJWT, userController.getAllUsers); //
+router.put("/user", userController.updateUser);
+router.delete("/user/:cpf", userController.deleteUser);
+router.post("/login", userController.loginUser);
 
-const organizadorController = require("../controllers/organizadorController")
 //organizador
-router.post('/organizador', organizadorController.createOrganizador);
-router.get('/organizador', organizadorController.getAllOrganizador);
-router.put('/organizador', organizadorController.updateOrganizador);
-router.delete('/organizador/:id', organizadorController.deleteOrganizador);
+router.post("/organizador", organizadorController.createOrganizador);
+router.get("/organizador", organizadorController.getAllOrganizador);
+router.put("/organizador", organizadorController.updateOrganizador);
+router.delete("/organizador/:id", organizadorController.deleteOrganizador);
 
-
-const ingressoController = require("../controllers/ingressoController")
 //Rotas ingresso controler
-router.post('/ingresso', ingressoController.createIngresso);
-router.get('/ingresso', ingressoController.getAllIngresso);
-router.put('/ingresso', ingressoController.updateIngresso);
-router.delete('/ingresso/:id',ingressoController.deleteIngresso);
-
+router.post("/ingresso", ingressoController.createIngresso);
+router.get("/ingresso", ingressoController.getAllIngresso);
+router.put("/ingresso", ingressoController.updateIngresso);
+router.delete("/ingresso/:id", ingressoController.deleteIngresso);
 
 //rotas eventoController
-const eventoController = require("../controllers/eventoController")
-router.post('/evento', eventoController.createEvento);
-router.get('/evento', verifyJWT, eventoController.getAllEventos);
-router.put('/evento', eventoController.updateEvento);
-router.delete('/evento/:id', eventoController.deleteEvento);
-router.get('/evento/data',verifyJWT, eventoController.getEventosPorData);
-router.get('/evento/proximo', eventoController.getEventosdia);
-module.exports = router
+router.post("/evento", eventoController.createEvento);
+router.get("/evento", verifyJWT, eventoController.getAllEventos);
+router.put("/evento", eventoController.updateEvento);
+router.delete("/evento/:id", eventoController.deleteEvento);
+router.get("/evento/data", verifyJWT, eventoController.getEventosPorData);
+router.get("/evento/proximo", eventoController.getEventosdia);
+
+//compra
+router.post("/comprasimples", compraController.registrarCompraSimples);
+router.post("/compra", compraController. registrarCompra);
+
+module.exports = router;
